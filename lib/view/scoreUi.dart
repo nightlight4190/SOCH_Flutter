c8ff8d077a1ecaa527c45c30d4302acd52ef474e
yogendra0914 <yogensubedi1855@gmail.com>
2024-04-11 12:58:05 +0545

    final

--- lib/view/scoreUi.dart
+++ lib/view/scoreUi.dart
@@ -17,147 +17,117 @@ class ScoreUi extends StatelessWidget {
         color: Colors.white,
         child: Stack(
           children: [
-            Column(
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.5,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadiusDirectional.only(
-                        bottomEnd: Radius.circular(20),
-                        bottomStart: Radius.circular(20),
-                      ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadiusDirectional.only(
+                    bottomEnd: Radius.circular(20),
+                    bottomStart: Radius.circular(20),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    CircleContainer(
+                      left: -80,
+                      top: -20,
+                      height: 150,
+                      width: 150,
+                      color:
+                          Color.fromARGB(255, 128, 105, 169).withOpacity(0.5),
                     ),
-                    child: Stack(
-                      children: [
-                        CircleContainer(
-                          left: -80,
-                          top: -20,
-                          height: 150,
-                          width: 150,
-                          color: Color.fromARGB(255, 128, 105, 169)
-                              .withOpacity(0.5),
+                    CircleContainer(
+                      left: 80,
+                      top: -100,
+                      height: 150,
+                      width: 150,
+                      color:
+                          Color.fromARGB(255, 128, 105, 169).withOpacity(0.5),
+                    ),
+                    CircleContainer(
+                      left: 220,
+                      top: 30,
+                      height: 50,
+                      width: 50,
+                      color:
+                          Color.fromARGB(255, 128, 105, 169).withOpacity(0.5),
+                    ),
+                    CircleContainer(
+                      left: 320,
+                      top: 100,
+                      height: 200,
+                      width: 200,
+                      color:
+                          Color.fromARGB(255, 128, 105, 169).withOpacity(0.5),
+                    ),
+                    CircleContainer(
+                      left: 100,
+                      top: 100,
+                      height: 200,
+                      width: 200,
+                      color:
+                          Color.fromARGB(255, 128, 105, 169).withOpacity(0.6),
+                    ),
+                    CircleContainer(
+                      left: 120,
+                      top: 120,
+                      height: 160,
+                      width: 160,
+                      color:
+                          Color.fromARGB(255, 167, 156, 186).withOpacity(0.6),
+                    ),
+                    Positioned(
+                      top: 135,
+                      left: 135,
+                      child: Container(
+                        height: 130,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 236, 226, 226),
+                          borderRadius: BorderRadius.circular(200),
                         ),
-                        CircleContainer(
-                          left: 80,
-                          top: -100,
-                          height: 150,
-                          width: 150,
-                          color: Color.fromARGB(255, 128, 105, 169)
-                              .withOpacity(0.5),
-                        ),
-                        CircleContainer(
-                          left: 220,
-                          top: 30,
-                          height: 50,
-                          width: 50,
-                          color: Color.fromARGB(255, 128, 105, 169)
-                              .withOpacity(0.5),
-                        ),
-                        CircleContainer(
-                          left: 320,
-                          top: 100,
-                          height: 200,
-                          width: 200,
-                          color: Color.fromARGB(255, 128, 105, 169)
-                              .withOpacity(0.5),
-                        ),
-                        CircleContainer(
-                          left: 100,
-                          top: 100,
-                          height: 200,
-                          width: 200,
-                          color: Color.fromARGB(255, 128, 105, 169)
-                              .withOpacity(0.6),
-                        ),
-                        CircleContainer(
-                          left: 120,
-                          top: 120,
-                          height: 160,
-                          width: 160,
-                          color: Color.fromARGB(255, 167, 156, 186)
-                              .withOpacity(0.6),
-                        ),
-                        Positioned(
-                          top: 135,
-                          left: 135,
-                          child: Container(
-                            height: 130,
-                            width: 130,
-                            decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 236, 226, 226),
-                              borderRadius: BorderRadius.circular(200),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Your Score',
+                              style: TextStyle(
+                                color: Colors.deepPurple,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15,
+                              ),
                             ),
-                            child: Column(
+                            Row(
                               mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Text(
-                                  'Your Score',
+                                  '150',
                                   style: TextStyle(
                                     color: Colors.deepPurple,
                                     fontWeight: FontWeight.w500,
-                                    fontSize: 15,
+                                    fontSize: 40,
                                   ),
                                 ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      '150',
-                                      style: TextStyle(
-                                        color: Colors.deepPurple,
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 40,
-                                      ),
-                                    ),
-                                    Text('pt'),
-                                  ],
+                                Text(
+                                  'pt',
+                                  style: TextStyle(
+                                    color: Colors.deepPurple,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ],
                             ),
-                          ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 120,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconElements(
-                      color1: Colors.green,
-                      icon1: Icons.replay,
-                      text1: 'Play Again',
-                      color2: Colors.blue,
-                      icon2: Icons.file_copy,
-                      text2: 'Generate Pdf',
-                    ),
-                    IconElements(
-                      color1: Colors.orange,
-                      icon1: Icons.remove_red_eye,
-                      text1: 'Review Answer',
-                      color2: Colors.purple,
-                      icon2: Icons.home_outlined,
-                      text2: 'Generate Pdf',
-                    ),
-                    IconElements(
-                      color1: Colors.indigo,
-                      icon1: Icons.share,
-                      text1: 'Play Again',
-                      color2: Colors.grey,
-                      icon2: Icons.leaderboard_outlined,
-                      text2: 'Generate Pdf',
+                      ),
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
             Align(
               alignment: Alignment.center,
@@ -174,6 +144,142 @@ class ScoreUi extends StatelessWidget {
                   ],
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              color: Colors.deepPurple,
+                              size: 15,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '100%',
+                                style: TextStyle(
+                                  color: Colors.deepPurple,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              color: Colors.green,
+                              size: 15,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '13',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              color: Colors.deepPurple,
+                              size: 15,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '20',
+                                style: TextStyle(
+                                  color: Colors.deepPurple,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              color: Colors.red,
+                              size: 15,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '07',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: 50,
+              ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 200,
+                  width: 400,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconElements(
+                        color1: Colors.green,
+                        icon1: Icons.replay,
+                        text1: 'Play Again',
+                        color2: Colors.blue,
+                        icon2: Icons.file_copy,
+                        text2: 'Generate Pdf',
+                      ),
+                      IconElements(
+                        color1: Colors.orange,
+                        icon1: Icons.remove_red_eye,
+                        text1: 'Review Answer',
+                        color2: Colors.purple,
+                        icon2: Icons.home_outlined,
+                        text2: 'Generate Pdf',
+                      ),
+                      IconElements(
+                        color1: Colors.indigo,
+                        icon1: Icons.share,
+                        text1: 'Play Again',
+                        color2: Colors.grey,
+                        icon2: Icons.leaderboard_outlined,
+                        text2: 'Generate Pdf',
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
