34d8c6a3701790ce2eb7783e361adbaf2bcdf04e
yogendra0914 <yogensubedi1855@gmail.com>
2024-04-15 19:31:36 +0545

    finished hw

--- lib/Day_6/homework/chat.dart
+++ lib/Day_6/homework/chat.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,176 +11,250 @@ class ChatScreen extends StatelessWidget {
     Color secondaryColor = Color.fromARGB(255, 233, 237, 245);
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 10,
-          left: 5,
-        ),
-        child: Container(
-          color: Color.fromARGB(255, 248, 247, 247),
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          // color: Colors.red,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 15.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Icon(
-                        Icons.arrow_back,
-                        color: primaryColor,
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'Alan Clark',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Poppins',
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          'Online',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            color: Colors.grey,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: Container(
-                        height: 35,
-                        width: 35,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(imgUrl),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(200),
+      body: Column(
+        children: [
+          Container(
+            color: Color.fromARGB(255, 248, 247, 247),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            // color: Colors.red,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: primaryColor,
+                          size: 26,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                  left: 20,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    chatContainer(
-                      height: 26,
-                      width: 60,
-                      text: 'Hi Alan ',
-                      radius: 14,
-                      color: secondaryColor,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    chatContainer(
-                      height: 55,
-                      width: MediaQuery.of(context).size.width * 0.52,
-                      text: 'I am Jason, here to help you find new friends',
-                      radius: 10,
-                      color: secondaryColor,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 28,
-                      width: MediaQuery.of(context).size.width * 0.52,
-                      decoration: BoxDecoration(
-                        color: secondaryColor,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8),
-                          topRight: Radius.circular(8),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          top: 4,
-                          left: 8,
-                        ),
-                        child: Text(
-                          'Select an interest to begin',
-                          style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w200,
-                              color: Colors.black54),
-                        ),
-                      ),
-                    ),
-                    InterestContainer(
-                      primaryColor: primaryColor,
-                      radius: 0,
-                      text: 'Travelling the world',
-                    ),
-                    InterestContainer(
-                      primaryColor: primaryColor,
-                      radius: 0,
-                      text: 'Photography',
-                    ),
-                    InterestContainer(
-                      primaryColor: primaryColor,
-                      radius: 8,
-                      text: 'Watching Movie',
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        right: 20,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                      Column(
                         children: [
-                          Container(
-                            height: 26,
-                            width: MediaQuery.of(context).size.width * 0.3,
-                            decoration: BoxDecoration(
-                              color: primaryColor,
-                              borderRadius: BorderRadius.circular(14),
+                          Text(
+                            'Alan Clark',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Poppins',
+                              fontSize: 20,
                             ),
-                            child: Center(
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 7),
-                                child: Text(
-                                  'Photography',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
+                          ),
+                          Text(
+                            'Online',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              color: Colors.grey,
+                              fontSize: 14,
                             ),
                           ),
                         ],
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(imgUrl),
+                              fit: BoxFit.cover,
+                            ),
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(200),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 40,
+                    left: 20,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      chatContainer(
+                        height: 35,
+                        width: 65,
+                        text: 'Hi Alan ',
+                        radius: 14,
+                        color: secondaryColor,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      chatContainer(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        text: 'I am Jason, here to help you find new friends.',
+                        radius: 10,
+                        color: secondaryColor,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        height: 35,
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            topRight: Radius.circular(8),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            top: 4,
+                            left: 8,
+                          ),
+                          child: Text(
+                            'Select an interest to begin',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w200,
+                                color: Colors.black54),
+                          ),
+                        ),
+                      ),
+                      InterestContainer(
+                        primaryColor: primaryColor,
+                        radius: 0,
+                        text: 'Travelling the world',
+                      ),
+                      InterestContainer(
+                        primaryColor: primaryColor,
+                        radius: 0,
+                        text: 'Photography',
+                      ),
+                      InterestContainer(
+                        primaryColor: primaryColor,
+                        radius: 8,
+                        text: 'Watching Movie',
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          right: 20,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              height: 35,
+                              width: MediaQuery.of(context).size.width * 0.3,
+                              decoration: BoxDecoration(
+                                color: primaryColor,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 7),
+                                  child: Text(
+                                    'Photography',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 200,
+                      ),
+                      Container(
+                        height: 35,
+                        width: 65,
+                        decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 7, right: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(
+                                  Icons.circle,
+                                  size: 10,
+                                  color: Color.fromARGB(255, 139, 136, 136),
+                                ),
+                                Icon(
+                                  Icons.circle,
+                                  size: 10,
+                                  color: Color.fromARGB(255, 189, 185, 185),
+                                ),
+                                Icon(
+                                  Icons.circle,
+                                  size: 10,
+                                  color: Color.fromARGB(255, 203, 200, 200),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width * 0.265,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20.0,
+                      right: 20,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.emoji_emotions_outlined,
+                          size: 30,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'Type message',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.send,
+                          color: primaryColor,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -204,8 +279,8 @@ class InterestContainer extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(bottom: 1.8),
       child: Container(
-        height: 28,
-        width: MediaQuery.of(context).size.width * 0.52,
+        height: 40,
+        width: MediaQuery.of(context).size.width * 0.6,
         decoration: BoxDecoration(
           color: Color.fromARGB(255, 255, 255, 255),
           borderRadius: BorderRadius.only(
@@ -215,13 +290,13 @@ class InterestContainer extends StatelessWidget {
         ),
         child: Padding(
           padding: const EdgeInsets.only(
-            top: 4,
+            top: 10,
             left: 12,
           ),
           child: Text(
             text,
             style: TextStyle(
-              fontSize: 13,
+              fontSize: 15,
               fontWeight: FontWeight.w500,
               color: primaryColor,
             ),
@@ -262,7 +337,10 @@ class chatContainer extends StatelessWidget {
           padding: const EdgeInsets.only(left: 7),
           child: Text(
             text,
-            style: TextStyle(fontSize: 12),
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
       ),
