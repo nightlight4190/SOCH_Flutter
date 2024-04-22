b5b6afdc9dc242e8db2e670f4b28a7286fed19f8
yogendra0914 <yogensubedi1855@gmail.com>
2024-04-22 17:51:44 +0545

    completed hw

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:day_5_hw/Day_10/classwork/PostData.dart';
+import 'package:day_5_hw/Day_10/homework/homelist.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: PostData(),
+      home: PersonList(),
     );
   }
 }
